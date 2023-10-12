a71677b9ac325d6ead23696a529e0a9651103c19
Darren Gardner <jamerican10@hotmail.comgit config --global user.name DGWonKanobi>
2023-10-11 23:35:42 -0400

    click button functions added

--- project-1-assessment/js/main.js
+++ project-1-assessment/js/main.js
@@ -1,11 +1,45 @@
-function updateCounters() {
-    const inputValue = parseFloat(document.getElementById('inputValue').value);
-    const plusCounter = document.querySelector('.counter.plus');
-    const minusCounter = document.querySelector('.counter.minus');
+// function updateCounters() {
+//     const inputValue = parseFloat(document.getElementById('inputValue').value);
+//     const plusCounter = document.querySelector('.counter.plus');
+//     const minusCounter = document.querySelector('.counter.minus');
+//     const resultBox = inputValue 
   
-    if (!isNaN(inputValue)) {
-      plusCounter.textContent = parseFloat(plusCounter.textContent) + inputValue;
-      minusCounter.textContent = parseFloat(minusCounter.textContent) - inputValue;
-    }
-    document.getElementById('inputValue').value = ''; // Clear the input field
-  }
+// //     if (!isNaN(inputValue)) {
+// //       plusCounter.textContent = parseFloat(plusCounter.textContent) + inputValue;
+// //       minusCounter.textContent = parseFloat(minusCounter.textContent) - inputValue;
+// //     }
+// //     document.getElementById('inputValue').value = ''; // Clear the input field
+//   }
+
+
+
+// Initialize variables to track the click count and input value
+let plusClickCount = 1;
+let minusClickCount = 1;
+let inputValue = 0; // Initial input value
+let resultValue = 0; // Initial result value
+
+// Function to handle plus button click
+function handlePlusButtonClick() {
+  plusClickCount++;
+  inputValue += plusClickCount;
+  updateResult();
+}
+
+// Function to handle minus button click
+function handleMinusButtonClick() {
+  minusClickCount++;
+  inputValue -= minusClickCount;
+  updateResult();
+}
+
+// Function to update the result and input value
+function updateResult() {
+  resultValue = inputValue;
+  document.getElementById('inputValue').value = inputValue;
+  document.getElementById('resultBox').textContent = resultValue;
+}
+
+// Add event listeners to plus and minus buttons
+document.getElementById('plusButton').addEventListener('click', handlePlusButtonClick);
+document.getElementById('minusButton').addEventListener('click', handleMinusButtonClick);
